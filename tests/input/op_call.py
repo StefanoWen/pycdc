882260041e97ffa7ccc2e6ee42c5ddec0086b7ba
MrDakik <urielsameh@gmail.com>
2024-05-20 20:43:19 +0300

    add tests for `CALL`

---
+++ tests/input/op_call.py
@@ -0,0 +1,14 @@
+def func(*args, **kwargs):
+	print('hello')
+
+a = 1
+func()
+func(1)
+func(a)
+func(b=1)
+func(1, b='2')
+func(1, 2)
+func(b='1', c='2')
+func(1, 2, b='3')
+func(1, b='2', c='3')
+func(1, 2, b='3', c='4')
